81ab0f28320e5a3535039fec1fd3056b742cd333
brunobsm88 <brunobsm88@gmail.com>
2015-03-30 17:15:13 -0300

    Meu Primeiro Projeto Github

---
+++ ConsoleTestes/GerandoToken128.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ConsoleTestes
+{
+    class GerandoToken128
+    {
+        static void sMain(string[] args)
+        {
+
+
+            //byte[] linkBytes = new byte[128]; 
+            //System.Security.Cryptography.RNGCryptoServiceProvider rngCrypto = new System.Security.Cryptography.RNGCryptoServiceProvider(); 
+            //rngCrypto.GetBytes(linkBytes); 
+            //string text128 = Convert.ToBase64String(linkBytes);
+            //string text128Enc = Uri.EscapeDataString(text128);
+
+            //int numeroBytes = System.Text.Encoding.UTF8.GetByteCount(text128);
+            //numeroBytes = System.Text.Encoding.UTF8.GetByteCount(text128Enc);
+
+            byte[] linkBytes = new byte[128];
+            var rngCrypto = new System.Security.Cryptography.RNGCryptoServiceProvider();
+            rngCrypto.GetBytes(linkBytes);
+            var text128 = Convert.ToBase64String(linkBytes);
+            var text128Enc = Uri.EscapeDataString(text128);
+
+            int numeroBytes = System.Text.Encoding.UTF8.GetByteCount(text128);
+            numeroBytes = System.Text.Encoding.UTF8.GetByteCount(text128Enc);
+
+            var bytes = new byte[64];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(bytes);
+            }
+            // and if you need it as a string... 
+            string hash1 = BitConverter.ToString(bytes);
+            // or maybe... 
+            string hash2 = BitConverter.ToString(bytes).Replace("-", "").ToLower();
+
+            numeroBytes = System.Text.Encoding.UTF8.GetByteCount(hash2);
+
+            var res = RandomString(128);
+            numeroBytes = System.Text.Encoding.UTF8.GetByteCount(res);
+
+            var cont = 1000000;
+
+            var lista = new List<string>();
+
+            while (cont > 0)
+            {
+                var resp = RandomString(128);
+
+                if (lista.Exists(p => p.Equals(resp)))
+                {
+                    Console.WriteLine("Erro Chave duplicada");
+                    Console.ReadLine();
+                }
+                lista.Add(resp);
+
+                cont--;
+            }
+
+
+            Console.WriteLine("Fim");
+            Console.ReadLine();
+        }
+        static string RandomString(int length, string allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+        {
+            if (length < 0) throw new ArgumentOutOfRangeException("length", "length cannot be less than zero.");
+            if (string.IsNullOrEmpty(allowedChars)) throw new ArgumentException("allowedChars may not be empty.");
+
+            const int byteSize = 0x100;
+            var allowedCharSet = new HashSet<char>(allowedChars).ToArray();
+            if (byteSize < allowedCharSet.Length) throw new ArgumentException(String.Format("allowedChars may contain no more than {0} characters.", byteSize));
+
+            // Guid.NewGuid and System.Random are not particularly random. By using a
+            // cryptographically-secure random number generator, the caller is always
+            // protected, regardless of use.
+            using (var rng = new System.Security.Cryptography.RNGCryptoServiceProvider())
+            {
+                var result = new StringBuilder();
+                var buf = new byte[128];
+                while (result.Length < length)
+                {
+                    rng.GetBytes(buf);
+                    for (var i = 0; i < buf.Length && result.Length < length; ++i)
+                    {
+                        // Divide the byte into allowedCharSet-sized groups. If the
+                        // random value falls into the last group and the last group is
+                        // too small to choose from the entire allowedCharSet, ignore
+                        // the value in order to avoid biasing the result.
+                        var outOfRangeStart = byteSize - (byteSize % allowedCharSet.Length);
+                        if (outOfRangeStart <= buf[i]) continue;
+                        result.Append(allowedCharSet[buf[i] % allowedCharSet.Length]);
+                    }
+                }
+                return result.ToString();
+            }
+        }
+    }
+}
---
+++ ConsoleTestes/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace ConsoleTestes
+{
+    class Program
+    {
+        public static List<string> Lista { get; set; }
+
+        static void Main(string[] args)
+        {
+            Lista = new List<string>();
+            var nome = "Bruno dos Santos Moura";
+            var ar = nome.Split(' ');
+            var tamanhoMaximo = 30;
+            var subDominio = string.Empty;
+
+            foreach (var s in ar)
+            {
+                if ((subDominio + s).Length <= 30)
+                {
+                    subDominio += s;
+                    if (!Lista.Exists(p => p.Contains(subDominio)))
+                    {
+                        Lista.Add(subDominio);
+                        Console.WriteLine(subDominio);
+                    }
+                }
+            }
+            subDominio = string.Empty;
+            foreach (var s in ar)
+            {
+                if ((subDominio + s).Length <= 27)
+                {
+                    subDominio += s;
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        var temp = subDominio + i.ToString();
+                        if (!Lista.Exists(p => p.Contains(temp)))
+                        {
+                            //subDominio = temp;
+                            Lista.Add(temp);
+                            Console.WriteLine(temp);
+                        }
+                    }
+                }
+            }
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
